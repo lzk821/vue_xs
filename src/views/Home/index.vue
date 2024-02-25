c3a68b7ff7961fc47b9a32954f1d24f26845d78c
lzk821 <lzkx821@gmail.com>
2024-02-25 21:47:09 +0800

    面板组件封装

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -6,6 +6,17 @@
     <HomeNew/>
     <HomeHot/>
     <HomeProduct/>
+    <!-- 测试面板组件 -->
+    <HomePanel title="新鲜好物" subTitle="新鲜好物 好多商品">
+      <div>
+        我是新鲜好物的推荐内容
+      </div>
+    </HomePanel>
+    <HomePanel title="人气推荐" subTitle="人气推荐 好多商品">
+      <div>
+        我是人气推荐的推荐内容
+      </div>
+    </HomePanel>
 </template>
 <script lang='ts'>
 export default {
@@ -18,6 +29,7 @@ import HomeCategory from './components/HomeCategory.vue'
 import HomeHot from './components/HomeHot.vue'
 import HomeNew from './components/HomeNew.vue'
 import HomeProduct from './components/HomeProduct.vue'
+import HomePanel from './components/HomePanel.vue'
 </script>
 <style scoped>
 </style>
