f3be3680c55b06cafb6565b588dc208acbdc406a
lzk821 <lzkx821@gmail.com>
2024-02-25 16:21:57 +0800

    吸顶导航交互

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,16 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Layout from '@/views/Layout/index.vue'
 import Login from '@/views/Login/index.vue'
-
+import home from '@/views/Layout/home.vue'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [{
     path:'/',
-    component:Layout
+    component:Layout,
+    children:[
+      {path:'',
+      component:home}
+    ]
   },
   {
     path:'/login',
