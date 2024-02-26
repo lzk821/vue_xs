33c050da4858e860499dc738593ad813f2a06f40
lzk821 <lzkx821@gmail.com>
2024-02-26 23:49:58 +0800

    home-轮播图实现

--- src/views/Category/index.vue
+++ src/views/Category/index.vue
@@ -2,6 +2,17 @@
   import { getmbxAPI } from '@/apis/category';
   import {onMounted, ref} from 'vue'
   import {useRoute} from 'vue-router'
+  import {getBannerAPI} from '@/apis/home'
+  const bannerList = ref([])
+  const getBanner =async ()=>{
+    const res = await getBannerAPI(
+      {distributionSite:'2'})
+    console.log(res)
+    bannerList.value = res.result
+  }
+  onMounted(()=>{
+    getBanner()
+  })
   const router = useRoute()
   const categoryDate = ref({})
   const getCategory = async()=>{
@@ -24,11 +35,31 @@
           <el-breadcrumb-item>{{ categoryDate.name }}</el-breadcrumb-item>
         </el-breadcrumb>
       </div>
+      <div class="home-banner">
+        <el-carousel height="500px">
+        <el-carousel-item v-for="item in bannerList" :key="item.id">
+        <img :src="item.imgUrl" alt="">
+        </el-carousel-item>
+        </el-carousel>
+      </div>
     </div>
   </div>
 </template>
 
 <style scoped lang="scss">
+.home-banner {
+  width: 1240px;
+  height: 500px;
+  // position: absolute;
+  left: 0;
+  top: 0;
+  z-index: 98;
+
+  img {
+    width: 100%;
+    height: 500px;
+  }
+}
 .top-category {
   h3 {
     font-size: 28px;
