ca1b29c2a0d4ce828e0a067ebdcd98d1941aac3d
lzk821 <lzkx821@gmail.com>
2024-03-04 13:12:07 +0800

    登录-表单校验-自定义校验规则

--- src/views/Login/index.vue
+++ src/views/Login/index.vue
@@ -2,7 +2,8 @@
 import {ref} from 'vue'
 const form = ref({
   account:'',
-  password:''
+  password:'',
+  agree:'fasle'
 })
 
 const rules = {
@@ -12,7 +13,16 @@ const rules = {
   password:[
     {required:true,message:'密码不能为空',trigger:'blur'},
     {min:8,max:14,message:'密码长度应该为8-14位',trigger:'blur'}
-  ]
+  ],
+  agree:[{
+    validator:(rule,value,callback)=>{
+      if(value){
+        callback()
+      } else {
+        callback(new Error("请勾选协议"))
+      }
+    }
+}]
 }
 </script>
 
@@ -46,8 +56,8 @@ const rules = {
               <el-form-item prop="password" label="密码">
                 <el-input v-model="form.password"/>
               </el-form-item>
-              <el-form-item label-width="22px">
-                <el-checkbox  size="large">
+              <el-form-item prop="agree" label-width="22px">
+                <el-checkbox  size="large" v-model="form.agree">
                   我已同意隐私条款和服务条款
                 </el-checkbox>
               </el-form-item>
