bcc4c6dbdfafa69163b5724d4dee7b8bbaadf711
lzk821 <lzkx821@gmail.com>
2024-03-05 00:44:20 +0800

    登录-pinia数据持久化

--- src/main.js
+++ src/main.js
@@ -7,16 +7,19 @@ import App from './App.vue'
 import router from './router'
 import {lazyPlugin} from '@/directives'
 import {componentsPlugin} from '@/components/index'
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
 
 // // 测试接口函数
 // import { getCategoryAPI } from './apis/testAPI'
 // getCategoryAPI().then(res =>{
 //     console.log(res)
 // })
-
+  
 const app = createApp(App)
-
-app.use(createPinia())
+const pinia = createPinia()
+// 注册持久化插件
+pinia.use(piniaPluginPersistedstate)
+app.use(pinia)
 app.use(router)
 app.use(lazyPlugin)
 app.use(componentsPlugin)
--- src/views/Login/index.vue
+++ src/views/Login/index.vue
@@ -1,37 +1,53 @@
 <script setup>
-import {ref} from 'vue'
-import 'element-plus/theme-chalk/el-message.css'
+
+
+// 表单校验（账号名+密码）
+
+import { ref } from 'vue'
+
 import { ElMessage } from 'element-plus'
-import {useRouter} from 'vue-router'
-import {useUserStore} from '@/stores/user'
+import 'element-plus/theme-chalk/el-message.css'
+import { useRouter } from 'vue-router'
+
+import { useUserStore } from '@/stores/userStore'
+
 const userStore = useUserStore()
-const router = useRouter()
+
+// 1. 准备表单对象
 const form = ref({
-  account:'',
-  password:'',
-  agree:'fasle'
+  account: 'xiaotuxian001',
+  password: '123456',
+  agree: true
 })
 
+// 2. 准备规则对象
 const rules = {
-  account:[
-    {required:true,message:'用户名不能为空',trigger:'blur'}
+  account: [
+    { required: true, message: '用户名不能为空', trigger: 'blur' }
   ],
-  password:[
-    {required:true,message:'密码不能为空',trigger:'blur'},
-    {min:6,max:14,message:'密码长度应该为6-14位',trigger:'blur'}
+  password: [
+    { required: true, message: '密码不能为空', trigger: 'blur' },
+    { min: 6, max: 14, message: '密码长度为6-14个字符', trigger: 'blur' },
   ],
-  agree:[{
-    validator:(rule,value,callback)=>{
-      if(value){
-        callback()
-      } else {
-        callback(new Error("请勾选协议"))
+  agree: [
+    {
+      validator: (rule, value, callback) => {
+        console.log(value)
+        // 自定义校验逻辑
+        // 勾选就通过 不勾选就不通过
+        if (value) {
+          callback()
+        } else {
+          callback(new Error('请勾选协议'))
+        }
       }
     }
-}]
+  ]
 }
 
+// 3. 获取form实例做统一校验
 const formRef = ref(null)
+const router = useRouter()
 const doLogin = () => {
   const { account, password } = form.value
   // 调用实例方法
@@ -41,14 +57,18 @@ const doLogin = () => {
     // 以valid做为判断条件 如果通过校验才执行登录逻辑
     if (valid) {
       // TODO LOGIN
-      await userStore.getUserInfo({account,password})
+      await userStore.getUserInfo({ account, password })
       // 1. 提示用户
-      ElMessage({type:'success',message:'登录成功'})
-      // 2.跳转首页
-      router.replace({path:'/'})
+      ElMessage({ type: 'success', message: '登录成功' })
+      // 2. 跳转首页
+      router.replace({ path: '/' })
     }
   })
 }
+
+// 1. 用户名和密码 只需要通过简单的配置（看文档的方式 - 复杂功能通过多个不同组件拆解）
+// 2. 同意协议  自定义规则  validator:(rule,value,callback)=>{}
+// 3. 统一校验  通过调用form实例的方法 validate -> true
 </script>
 
 
@@ -57,7 +77,7 @@ const doLogin = () => {
     <header class="login-header">
       <div class="container m-top-20">
         <h1 class="logo">
-          <RouterLink to="/">liash商城</RouterLink>
+          <RouterLink to="/">小兔鲜</RouterLink>
         </h1>
         <RouterLink class="entry" to="/">
           进入网站首页
@@ -73,16 +93,15 @@ const doLogin = () => {
         </nav>
         <div class="account-box">
           <div class="form">
-            <el-form :model="form" ref="formRef" :rules="rules" label-position="right" label-width="60px"
-              status-icon>
+            <el-form ref="formRef" :model="form" :rules="rules" label-position="right" label-width="60px" status-icon>
               <el-form-item prop="account" label="账户">
-                <el-input v-model="form.account"/>
+                <el-input v-model="form.account" />
               </el-form-item>
               <el-form-item prop="password" label="密码">
-                <el-input v-model="form.password"/>
+                <el-input v-model="form.password" />
               </el-form-item>
               <el-form-item prop="agree" label-width="22px">
-                <el-checkbox  size="large" v-model="form.agree">
+                <el-checkbox size="large" v-model="form.agree">
                   我已同意隐私条款和服务条款
                 </el-checkbox>
               </el-form-item>
@@ -104,7 +123,7 @@ const doLogin = () => {
           <a href="javascript:;">搜索推荐</a>
           <a href="javascript:;">友情链接</a>
         </p>
-        <p>CopyRight &copy; liash商城</p>
+        <p>CopyRight &copy; 小兔鲜儿</p>
       </div>
     </footer>
   </div>
