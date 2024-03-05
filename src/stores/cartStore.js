9c11d80fe8f45498c68050bc48b16ba9245ae59f
lzk821 <lzkx821@gmail.com>
2024-03-05 23:42:14 +0800

    本地头部购物车-删除功能

--- src/stores/cartStore.js
+++ src/stores/cartStore.js
@@ -9,12 +9,16 @@ export const useCartStore = defineStore('cart',()=>{
         item.count++
       }else{
         cartList.value.push(goods)
-      }
-
+      } 
     }
+    const delCart = (skuId) =>{
+        const idx = cartList.value.findIndex((item)=>skuId === item.skuId)
+        cartList.value.splice(idx,1)
+      }
     return {
         cartList,
-        addCart
+        addCart,
+        delCart
     }
 },
 {
