c98582fe73bb3c8051eebb3cf28f0ac5906bbfd5
lzk821 <lzkx821@gmail.com>
2024-03-05 21:18:01 +0800

    登录-退出登录业务实现

--- src/views/Layout/components/LayoutNav.vue
+++ src/views/Layout/components/LayoutNav.vue
@@ -1,6 +1,13 @@
 <script setup>
 import {useUserStore} from '@/stores/userStore'
 const userStore = useUserStore()
+import {useRouter} from 'vue-router'
+const router = useRouter()
+const confirm = ()=>{
+  console.log('被调用了');
+  userStore.clearUserInfo()
+  router.replace('/login')
+}
 </script>
 
 <template>
@@ -10,7 +17,7 @@ const userStore = useUserStore()
         <template v-if="userStore.userInfo.token">
           <li><a href="javascript:;"><i class="iconfont icon-user"></i>{{userStore.userInfo.account}}</a></li>
           <li>
-            <el-popconfirm title="确认退出吗?" confirm-button-text="确认" cancel-button-text="取消">
+            <el-popconfirm @confirm="confirm" title="确认退出吗?" confirm-button-text="确认" cancel-button-text="取消">
               <template #reference>
                 <a href="javascript:;">退出登录</a>
               </template>
