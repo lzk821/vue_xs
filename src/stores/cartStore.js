5a2e746c18eaf73a792e3f2679646c138887b804
lzk821 <lzkx821@gmail.com>
2024-03-05 23:55:42 +0800

    本地购物车-头部购物车-计算逻辑

--- src/stores/cartStore.js
+++ src/stores/cartStore.js
@@ -1,5 +1,5 @@
 import { defineStore } from 'pinia'
-import {ref} from 'vue'
+import {ref,computed} from 'vue'
 
 export const useCartStore = defineStore('cart',()=>{
     const cartList = ref([])
@@ -15,10 +15,17 @@ export const useCartStore = defineStore('cart',()=>{
         const idx = cartList.value.findIndex((item)=>skuId === item.skuId)
         cartList.value.splice(idx,1)
       }
+
+    // 1.总数
+      const allCount=computed(()=>cartList.value.reduce((a,c)=>a+c.count,0))
+    // 1.总价
+    const allPrice=computed(()=>cartList.value.reduce((a,c)=>a+c.count*c.price,0))
     return {
         cartList,
         addCart,
-        delCart
+        delCart,
+        allCount,
+        allPrice
     }
 },
 {
